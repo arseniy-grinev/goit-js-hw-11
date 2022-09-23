23d6c1a1ec3921b69fd9a07297da4976e60870b0
arseniy-grinev <arseniy.grinev@gmail.com>
2022-09-23 13:00:53 +0300

    add scroll

--- src/index.js
+++ src/index.js
@@ -89,3 +89,49 @@ function slowScroll() {
     behavior: 'smooth',
   });
 }
+
+(() => {
+  window.addEventListener('scroll', throttle(checkPosition, 250))
+  window.addEventListener('resize', throttle(checkPosition, 250))
+})()
+
+function throttle(callee, timeout) {
+  let timer = null
+
+  return function perform(...args) {
+    if (timer) return
+
+    timer = setTimeout(() => {
+      callee(...args)
+
+      clearTimeout(timer)
+      timer = null
+    }, timeout)
+  }
+}
+
+
+function checkPosition() {
+  // Нам потребуется знать высоту документа и высоту экрана:
+  const height = document.body.offsetHeight
+  const screenHeight = window.innerHeight
+
+  // Они могут отличаться: если на странице много контента,
+  // высота документа будет больше высоты экрана (отсюда и скролл).
+
+  // Записываем, сколько пикселей пользователь уже проскроллил:
+  const scrolled = window.scrollY
+
+  // Обозначим порог, по приближении к которому
+  // будем вызывать какое-то действие.
+  // В нашем случае — четверть экрана до конца страницы:
+  const threshold = height - screenHeight / 4
+
+  // Отслеживаем, где находится низ экрана относительно страницы:
+  const position = scrolled + screenHeight
+
+  if (position >= threshold) {
+    onLoadMore();
+    // Если мы пересекли полосу-порог, вызываем нужное действие.
+  }
+}
